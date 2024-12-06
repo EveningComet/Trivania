a7e5ccc225f4512a1a8b52d27334ced73b9a88ad
EveningComet <165712308+EveningComet@users.noreply.github.com>
2024-12-06 18:20:00 -0500

    Quick Tweaks
    Some quick tweaks.

--- Scripts/Utils/Utils.gd
+++ Scripts/Utils/Utils.gd
@@ -1,7 +1,7 @@
 ## Stores various helper functions.
 class_name Utils extends Node
 
-## Checks the passed node to see if it contains a node of the passed type.
+## Checks the passed parent node to see if it contains a node of the passed type.
 static func get_node_of_type(parent: Node, type):
 	for child in parent.get_children():
 		if is_instance_of(child, type):
--- Scripts/World/Area Event/AreaEventTrigger.gd
+++ Scripts/World/Area Event/AreaEventTrigger.gd
@@ -1,14 +1,25 @@
 ## Notifies something when the player enters this area.
 class_name AreaEventTrigger extends Area3D
 
+signal player_entered(player: Player)
+signal player_exited(player: Player)
+
+## Should this destroy itself after it has done its job?
+@export var one_shot: bool = false
+
 func _ready() -> void:
 	body_entered.connect( _on_body_entered )
 	body_exited.connect( _on_body_exited )
 
 func _on_body_entered(body) -> void:
 	if body is Player:
-		pass
+		if OS.is_debug_build() == true:
+			print("AreaEventTrigger :: Player entered.")
+		
+		player_entered.emit(body)
+		if one_shot == true:
+			queue_free()
 
 func _on_body_exited(body) -> void:
 	if body is Player:
-		pass
+		player_exited.emit(body)
